69be53788a90c18246f2553f9e38cea3224b79dd
Ikumi Shimizu <193s@users.noreply.github.com>
2015-01-17 20:35:00 +0900

    refactoring

--- scsh.scala
+++ scsh.scala
@@ -8,6 +8,7 @@ import scala.util.Properties
 import scala.collection.mutable
 import scala.sys.process.{Process, ProcessLogger}
 
+// ============================================ //
 
 val logger = ProcessLogger (
   out => System.out.println(out),
@@ -17,21 +18,108 @@ var pos = new File(".")
 
 // command execution
 def system(cmd: Seq[String]) = Process(cmd, pos) ! logger
-def parse(cmd: String): List[String] =
-  """\$\{([a-zA-Z_]+)\}""".r
-  .replaceAllIn(cmd, m => Matcher.quoteReplacement(env.get(m.group(1))))
-  .replace("\\ ", "\\$space")
-  .split(' ')
-  .map { _.trim.replace("\\$space", " ") }
-  .toList
 
-def getFile(file: File)(name: String) = new File(file.toURI.resolve(name))
-// read a line with prompt
-def readLine(prompt: String) = {
-  print(prompt)
-  StdIn.readLine()
+
+Runner.run()
+
+
+// ============================================ //
+
+
+object Runner {
+  val alias = new Alias()
+  val conf = new Conf(".scshrc")
+  val env = new Env()
+
+  // command execution
+  def eval(input: String): Boolean =
+    parse(input) match {
+      case Nil => true // do nothing
+
+      // shell built-in commands
+      case "exit" :: xs =>
+        println("Program will exit...")
+        sys.exit(0)
+        true
+
+      // assignment
+      case name :: "=" :: value :: _ =>
+        println(s"+ $name -> $value")
+        env.values += name -> value
+        true
+
+
+      case "cd" :: Nil => true // do nothing
+      case "cd" :: x :: _ =>
+        val dest = MyUtil.getFile(pos)(x)
+        if (!dest.isDirectory) {
+          println("cd: no such directory")
+          false
+        } else {
+          pos = dest
+          true
+        }
+
+      case "alias" :: Nil =>
+        println(alias.table.mkString("\n"))
+        true
+
+      case "alias" :: x :: _ =>
+        if (!x.contains('=')) {
+          if (alias.table.contains(x)) {
+            println(s"$x=${alias.table(x)}")
+            true
+          } else false
+        } else {
+          val p = alias.splitByEq(x)
+          println(s"alias: ${p._1} -> ${p._2}")
+          alias.table += p
+          true
+        }
+
+      case in =>
+        // result (0 or else)
+        val result = try system(alias.aliased(in))
+        catch {
+          case e: java.io.IOException =>
+            println(s"permission denied: $input")
+            1
+        }
+        result == 0
+    }
+
+  def run() {
+    conf.eval()
+
+    while (true) {
+      val input = StdIn.readLine(env.values("prompt"))
+      eval(input)
+    }
+  }
+  def parse(cmd: String): List[String] =
+    """\$\{([a-zA-Z_]+)\}""".r
+    .replaceAllIn(cmd, m => Matcher.quoteReplacement(env.get(m.group(1))))
+    .replace("\\ ", "\\$space")
+    .split(' ')
+    .map { _.trim.replace("\\$space", " ") }
+    .toList
+
+
+  // Config
+  class Conf(file: String) {
+    def eval(): Unit =
+      for (l <- Source.fromFile(file).getLines) Runner.eval(l)
+  }
 }
 
+
+object MyUtil {
+  // get file by given path
+  def getFile(file: File)(name: String) = new File(file.toURI.resolve(name))
+}
+
+
+// Environment
 class Env {
   import scala.io.AnsiColor._
   private val p = RED + "$ " + RESET
@@ -41,12 +129,8 @@ class Env {
 }
 
 
-// config
-class Conf {
-  val file = new File(".scshrc")
-  def load(): Unit =
-    for (l <- Source.fromFile(file).getLines) run(l)
-}
+
+
 
 
 class Alias {
@@ -55,7 +139,7 @@ class Alias {
   // returns aliased list
   def aliased(cmd: List[String]): List[String] = cmd match {
     case Nil     => Nil
-    case x :: xs => parse(aliased(x)) ::: xs
+    case x :: xs => Runner.parse(aliased(x)) ::: xs
   }
   // returns aliased string
   def aliased(cmd: String): String = table.getOrElse(cmd, cmd)
@@ -63,73 +147,5 @@ class Alias {
   // split string by the first '='
   def splitByEq(str: String) =
     str.splitAt(str.indexOf('=')) match { case (l, r) => (l, r.tail) }
-}
-
-
-val alias = new Alias()
-val conf = new Conf()
-val env = new Env()
-// command execution
-def run(input: String): Boolean =
-  parse(input) match {
-    case Nil => true // do nothing
-
-    // shell built-in commands
-    case "exit" :: xs =>
-      println("Program will exit...")
-      sys.exit(0)
-      true
-
-    // assignment
-    case name :: "=" :: value :: _ =>
-      println(s"+ $name -> $value")
-      env.values += name -> value
-      true
-
-
-    case "cd" :: Nil => true // do nothing
-    case "cd" :: x :: _ =>
-      val dest = getFile(pos)(x)
-      if (!dest.isDirectory) {
-        println("cd: no such directory")
-        false
-      } else {
-        pos = dest
-        true
-      }
-
-    case "alias" :: Nil =>
-      println(alias.table.mkString("\n"))
-      true
-
-    case "alias" :: x :: _ =>
-      if (!x.contains('=')) {
-        if (alias.table.contains(x)) {
-          println(s"$x=${alias.table(x)}")
-          true
-        } else false
-      } else {
-        val p = alias.splitByEq(x)
-        println(s"alias: ${p._1} -> ${p._2}")
-        alias.table += p
-        true
-      }
-
-    case in =>
-      // result (0 or else)
-      val result = try system(alias.aliased(in))
-      catch {
-        case e: java.io.IOException =>
-          println(s"permission denied: $input")
-          1
-      }
-      result == 0
-  }
-
-conf.load()
-
-while (true) {
-  val input = readLine(env.values("prompt"))
-  run(input)
 }
 
